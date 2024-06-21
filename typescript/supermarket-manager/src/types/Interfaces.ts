6851fd2fe6f51fc8e380c5466ffeb92b471cc0a0
Tassio-Med <tassiohfmed@gmail.como>
2024-06-21 15:43:54 -0300

    feat: IProduct, ICustomer and ISale interfaces created

--- typescript/supermarket-manager/src/types/Interfaces.ts
+++ typescript/supermarket-manager/src/types/Interfaces.ts
@@ -1,20 +1,20 @@
 import { ProductCategory } from "./Enums";
 import { UUID } from "./Types";
 
-export  interface IProduct {
+export interface IProduct {
   id: UUID;
   name: string;  
   price: number;  
   category: ProductCategory;
   stock: number;
 }
-export  interface ICustomer {
+export interface ICustomer {
   id: UUID;
   name: string;  
   email: string;  
   phone: number;
 }
-export  interface ISale {
+export interface ISale {
   id: UUID;
   customer: ICustomer;  
   products: Array<{ product: IProduct, quantity: number }>;
