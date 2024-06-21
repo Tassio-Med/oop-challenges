97d90bef797f89ecc627343dfb429e1956d0de20
Tassio-Med <tassiohfmed@gmail.como>
2024-06-21 15:51:27 -0300

    feat: file for clean and centralized import

---
+++ typescript/supermarket-manager/src/models/index.ts
@@ -0,0 +1,4 @@
+// Importação centralizada e organizada das entidades definidas na pasta.
+export { Product } from './Product';
+export { Customer } from './Customer';
+export { Sale } from './Sale';
