a1304efb2c6c08debcecd56650f68026ba86ca79
Tassio-Med <tassiohfmed@gmail.como>
2024-06-21 15:47:54 -0300

    feat: Customer model created

---
+++ typescript/supermarket-manager/src/models/Customer.ts
@@ -0,0 +1,10 @@
+import { ICustomer } from "../types/Interfaces";
+
+export class Customer implements ICustomer {
+  constructor(
+    public id: string,
+    public name: string,
+    public email: string,
+    public phone: number,
+  ){}
+}
